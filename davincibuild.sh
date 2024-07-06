50e38e2ea6945bd5d4216977b715b8fc909819ce
tagicmi <166647160+tagicmi@users.noreply.github.com>
2024-07-06 14:23:45 +0300

    Update and rename sweetbuild.sh to davincibuild.sh

--- davincibuild.sh
+++ davincibuild.sh
@@ -6,8 +6,8 @@ set -e
 PYTHON_COMMAND=python2.7
 rm -rf workspace/Build/F11/DEBUG_GCC5/FV/Ffs/7E374E25-8E01-4FEE-87F2-390C23C606CDFVMAIN
 # not actually GCC5; it's GCC7 on Ubuntu 18.04.
-GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p F11/sweet.dsc
+GCC5_AARCH64_PREFIX=aarch64-linux-gnu- build -s -n 0 -a AARCH64 -t GCC5 -p F11/davinci.dsc
 gzip -c < Build/F11/DEBUG_GCC5/FV/F11_UEFI.fd >uefi_image5
-cat sweet.dtb >> uefi_image5
-abootimg --create sweet_uefi.img -k uefi_image5 -r ramdisk-null -f bootimg.cfg
+cat davinci.dtb >> uefi_image5
+abootimg --create davinci_uefi_v1.img -k uefi_image5 -r ramdisk-null -f bootimg.cfg
 rm uefi_image* Build/ -rf
